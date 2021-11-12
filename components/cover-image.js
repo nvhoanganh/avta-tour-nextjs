a05995943c6e9e24bebcb4a7c2d1ab289f7c4fdc
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-11-13 01:45:44 +1100

    update

--- components/cover-image.js
+++ components/cover-image.js
@@ -8,8 +8,8 @@ export default function CoverImage({ title, url, slug }) {
       width={2000}
       height={1000}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
+      className={cn('rounded-xl shadow-sm', {
+        'hover:shadow-md transition-shadow duration-200': slug,
       })}
       src={url}
     />
--- components/post-header.js
+++ components/post-header.js
@@ -13,7 +13,7 @@ export default function PostHeader({ title, coverImage, date, author }) {
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} url={coverImage.url} />
       </div>
-      <div className="max-w-2xl mx-auto">
+      <div className="max-w-3xl mx-auto">
         <div className="block md:hidden mb-6">
           {author && <Avatar name={author.name} picture={author.picture} />}
         </div>
