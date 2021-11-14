7b647463fff06c4524379d0b0dc4a5db85ac5ae5
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-11-14 17:26:42 +1100

    pull players

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,50 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
+import Container from '../components/container';
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPostsForHome, getAllPlayers } from '../lib/api';
+import Head from 'next/head';
 
-export default function Index({ preview, allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout preview={preview}>
-        <Head>
-          <title>AVTA - Australia Vietnamese Tennis Association</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
+export default function Index({ preview, allPosts, allPlayers }) {
+	const heroPost = allPosts[0];
+	const morePosts = allPosts.slice(1);
+
+	console.log('players', allPlayers);
+	console.log('posts', morePosts);
+
+	return (
+		<>
+			<Layout preview={preview}>
+				<Head>
+					<title>
+						AVTA - Australia Vietnamese Tennis Association
+					</title>
+				</Head>
+				<Container>
+					<Intro />
+					{heroPost && (
+						<HeroPost
+							title={heroPost.title}
+							coverImage={heroPost.coverImage}
+							date={heroPost.date}
+							author={heroPost.author}
+							slug={heroPost.slug}
+							excerpt={heroPost.excerpt}
+						/>
+					)}
+					{morePosts.length > 0 && <MoreStories posts={morePosts} />}
+				</Container>
+			</Layout>
+		</>
+	);
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = (await getAllPostsForHome(preview)) ?? []
-  return {
-    props: { preview, allPosts },
-    revalidate: 60
-  }
+	const allPosts = (await getAllPostsForHome(preview)) ?? [];
+	const allPlayers = (await getAllPlayers(preview)) ?? [];
+	return {
+		props: { preview, allPosts, allPlayers },
+		revalidate: 60,
+	};
 }
