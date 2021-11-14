771ee0a1ab42e66c4a46b0d4a93e7281fe497333
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-11-14 18:33:42 +1100

    load competition

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,21 @@ import MoreStories from '../components/more-stories';
 import HeroPost from '../components/hero-post';
 import Intro from '../components/intro';
 import Layout from '../components/layout';
-import { getAllPostsForHome, getAllPlayers } from '../lib/api';
+import {
+	getAllCompetitionsForHome,
+	getAllPostsForHome,
+	getAllPlayers,
+	getTop10Players,
+} from '../lib/api';
 import Head from 'next/head';
 
-export default function Index({ preview, allPosts, allPlayers }) {
+export default function Index({ preview, allPosts, allPlayers, competittions }) {
 	const heroPost = allPosts[0];
 	const morePosts = allPosts.slice(1);
 
 	console.log('players', allPlayers);
 	console.log('posts', morePosts);
+	console.log('competittions', competittions);
 
 	return (
 		<>
@@ -42,9 +48,10 @@ export default function Index({ preview, allPosts, allPlayers }) {
 
 export async function getStaticProps({ preview = false }) {
 	const allPosts = (await getAllPostsForHome(preview)) ?? [];
-	const allPlayers = (await getAllPlayers(preview)) ?? [];
+	const allPlayers = (await getTop10Players(preview)) ?? [];
+	const competittions = (await getAllCompetitionsForHome(preview)) ?? [];
 	return {
-		props: { preview, allPosts, allPlayers },
+		props: { preview, allPosts, allPlayers, competittions },
 		revalidate: 60,
 	};
 }
