65c732069cd49cf05e5c06463e80f3d76ad6bcf2
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-11-17 02:00:43 +1100

    comp

--- pages/competitions/[slug].js
+++ pages/competitions/[slug].js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Head from 'next/head';
 import ErrorPage from 'next/error';
 import ContentfulImage from '../../components/contentful-image';
@@ -113,21 +114,23 @@ export default function Competition({ competition, preview }) {
 													</div>
 												</div>
 
-												{competition.active ? (
-													<div className='w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center'>
-														<div className='py-6 px-3 mt-32 sm:mt-0'>
-															<a
-																href={
-																	competition.applicationGForm
-																}
-																target='_blank'
-																className='bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150'
-															>
-																Apply Now
-															</a>
-														</div>
+												<div className='w-full lg:w-4/12 px-4 lg:order-3 lg:text-right text-center lg:self-center'>
+													<div className='py-6 px-3 mt-24 sm:mt-0'>
+														<a
+															href={
+																competition.active
+																	? competition.applicationGForm
+																	: competition.resultSheets
+															}
+															target='_blank'
+															className='bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150'
+														>
+															{competition.active
+																? 'Apply Now'
+																: 'View Results'}
+														</a>
 													</div>
-												) : null}
+												</div>
 
 												<div className='w-full lg:w-4/12 px-4 lg:order-1'>
 													<div className='flex justify-center py-4 lg:pt-4 pt-8'>
@@ -160,17 +163,13 @@ export default function Competition({ competition, preview }) {
 																	teamJoined}
 															</span>
 															<span className='text-sm text-gray-400'>
-																Spots Remaining
+																Spots
 															</span>
 														</div>
 													</div>
 												</div>
 											</div>
-
-											<div className='text-center mt-10'>
-												<h3 className='text-4xl font-semibold leading-normal mb-2 text-gray-700 mb-2'>
-													{competition.title}
-												</h3>
+											<div className='text-center'>
 												<div className='text-sm leading-normal mt-0 mb-2 text-gray-400 font-bold uppercase'>
 													<i className='fas fa-map-marker-alt mr-2 text-lg text-gray-400'></i>{' '}
 													{competition.club},{' '}
@@ -182,21 +181,34 @@ export default function Competition({ competition, preview }) {
 												</div>
 											</div>
 
-											<h2 className='mb-8 mt-10 text-2xl md:text-3xl font-bold tracking-tighter leading-tight mx-auto'>
-												Registered Teams
-											</h2>
-
-											<div className='mx-auto mt-3'>
-												<TeamsCard
-													teams={competition.teams}
-												/>
-											</div>
-
-											<div className='mt-10 py-10 border-t border-gray-200 text-center'>
+											<div className='mx-0 md:mx-4'>
+												<h3 className='mt-10 text-2xl md:text-3xl font-bold tracking-tighter leading-tight mx-auto'>
+													{competition.title}
+												</h3>
+												<div className='prose text-lg mt-10'>
+													{documentToReactComponents(
+														competition.description
+															.json
+													)}
+												</div>
+												<h2 className='mt-20 text-2xl md:text-3xl font-bold tracking-tighter leading-tight mx-auto'>
+													Registered Teams
+												</h2>
+												<div className='mx-auto mt-10'>
+													<TeamsCard
+														teams={
+															competition.teams
+														}
+													/>
+												</div>
+												{/* <div className='mt-10 py-10 border-t border-gray-200 text-center'>
 												<div className='flex flex-wrap justify-center'>
 													<div className='w-full lg:w-9/12 px-4'>
 														<a
-															href='#pablo'
+															href={
+																competition.resultSheets
+															}
+															target='_blank'
 															className='font-normal text-blue-500'
 															onClick={(e) =>
 																e.preventDefault()
@@ -206,6 +218,7 @@ export default function Competition({ competition, preview }) {
 														</a>
 													</div>
 												</div>
+											</div> */}
 											</div>
 										</div>
 									</div>
