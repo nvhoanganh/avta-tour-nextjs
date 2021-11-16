d00e0e2b7afccd1cd0416a525bbe999d6779e313
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-11-17 01:24:31 +1100

    registered Teams

---
+++ components/Cards/TeamsCard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Link from 'next/link';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+import ContentfulImage from '../contentful-image';
+
+// components
+
+import TableDropdown from '../Dropdowns/TableDropdown.js';
+import CardStats from '../Cards/CardStats.js';
+
+export default function TeamsCard({ color, teams }) {
+	return (
+		<>
+			<div className='grid grid-cols-1 md:grid-cols-2 md:gap-x-10 lg:gap-x-16 gap-y-5 mb-32'>
+				{teams.map((team) => {
+					let description =
+						team.playersCollection.items[0].fullName +
+						`(${team.playersCollection.items[0].avtaPoint})`;
+
+					description +=
+						' - ' +
+						team.playersCollection.items[1].fullName +
+						`(${team.playersCollection.items[1].avtaPoint})`;
+
+					return (
+						<CardStats
+							key={team.name}
+							link={`#`}
+							statSubtitle={
+								team.playersCollection.items[0].homeClub || 'Unknown Club'
+							}
+							statTitle={team.name}
+							statArrow=''
+							statPercent={
+								team.playersCollection.items[0].avtaPoint +
+								team.playersCollection.items[1].avtaPoint +
+								' pt.'
+							}
+							statPercentColor='text-green-500'
+							statDescripiron={description}
+							statIconName='fas fa-user-check'
+							statIconColor='bg-green-500'
+						/>
+					);
+				})}
+			</div>
+		</>
+	);
+}
+
+TeamsCard.defaultProps = {
+	color: 'light',
+	teams: [],
+};
+
+TeamsCard.propTypes = {
+	color: PropTypes.oneOf(['light', 'dark']),
+};
--- pages/competitions/[slug].js
+++ pages/competitions/[slug].js
@@ -15,6 +15,7 @@ import PostTitle from '../../components/post-title';
 import Intro from '../../components/intro';
 import IndexNavbar from '../../components/Navbars/IndexNavbar.js';
 import Navbar from '../../components/Navbars/AuthNavbar.js';
+import TeamsCard from '../../components/Cards/TeamsCard.js';
 
 export default function Competition({ competition, preview }) {
 	const router = useRouter();
@@ -23,8 +24,15 @@ export default function Competition({ competition, preview }) {
 		return <ErrorPage statusCode={404} />;
 	}
 
-	console.log(competition);
 	const teamJoined = competition.teamsCollection?.items?.length || 0;
+
+	const totalPoints = competition.teams.reduce((previousTotal, team) => {
+		return (
+			previousTotal +
+			team.playersCollection.items[0].avtaPoint +
+			team.playersCollection.items[1].avtaPoint
+		);
+	}, 0);
 
 	return (
 		<Layout preview={preview}>
@@ -108,12 +116,15 @@ export default function Competition({ competition, preview }) {
 												{competition.active ? (
 													<div className='w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center'>
 														<div className='py-6 px-3 mt-32 sm:mt-0'>
-															<button
+															<a
+																href={
+																	competition.applicationGForm
+																}
+																target='_blank'
 																className='bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150'
-																type='button'
 															>
 																Apply Now
-															</button>
+															</a>
 														</div>
 													</div>
 												) : null}
@@ -129,15 +140,22 @@ export default function Competition({ competition, preview }) {
 															</span>
 														</div>
 														<div className='mr-4 p-3 text-center'>
-															<span className='text-xl font-bold block uppercase tracking-wide text-gray-600'>
-																10
+															<span className='text-xl font-bold block uppercase tracking-wide text-green-700'>
+																{teamJoined > 0
+																	? (
+																			totalPoints /
+																			teamJoined
+																	  ).toFixed(
+																			2
+																	  )
+																	: '-'}
 															</span>
 															<span className='text-sm text-gray-400'>
 																Avg Point
 															</span>
 														</div>
 														<div className='lg:mr-4 p-3 text-center'>
-															<span className='text-xl font-bold block uppercase tracking-wide text-gray-600'>
+															<span className='text-xl font-bold block uppercase tracking-wide text-red-600'>
 																{16 -
 																	teamJoined}
 															</span>
@@ -157,10 +175,23 @@ export default function Competition({ competition, preview }) {
 													<i className='fas fa-map-marker-alt mr-2 text-lg text-gray-400'></i>{' '}
 													{competition.club},{' '}
 													<DateComponent
-														dateString={competition.date}
+														dateString={
+															competition.date
+														}
 													/>
 												</div>
 											</div>
+
+											<h2 className='mb-8 mt-10 text-2xl md:text-3xl font-bold tracking-tighter leading-tight mx-auto'>
+												Registered Teams
+											</h2>
+
+											<div className='mx-auto mt-3'>
+												<TeamsCard
+													teams={competition.teams}
+												/>
+											</div>
+
 											<div className='mt-10 py-10 border-t border-gray-200 text-center'>
 												<div className='flex flex-wrap justify-center'>
 													<div className='w-full lg:w-9/12 px-4'>
