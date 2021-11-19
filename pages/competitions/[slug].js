9864a6b2786164eaef91172d5e980c1241b4f232
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-11-19 18:54:14 +1100

    final changes

--- pages/competitions/[slug].js
+++ pages/competitions/[slug].js
@@ -118,20 +118,32 @@ export default function Competition({ competition, preview }) {
 												</div>
 
 												<div className='w-full lg:w-4/12 px-4 lg:order-3 lg:text-right text-center lg:self-center'>
-													<div className='py-6 px-3 mt-24 sm:mt-0'>
-														<a
-															href={
-																competition.active
-																	? competition.applicationGForm
-																	: competition.resultSheets
-															}
-															target='_blank'
-															className='bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150'
-														>
-															{competition.active
-																? 'Apply Now'
-																: 'View Results'}
-														</a>
+													<div className='py-6 px-3 mt-24 sm:mt-0 flex flex-col sm:flex-row justify-end'>
+														{
+															competition.active
+															? <a
+																href={
+																	competition.applicationGForm
+																}
+																target='_blank'
+																className='bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150'
+															>
+																Apply Now
+															</a>
+															: <span className='text-gray-500'>Tournament Completed</span>
+														}
+														{
+															competition.active
+															&& <a
+																href={
+																	competition.googleForm
+																}
+																target='_blank'
+																className='bg-gray-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150'
+															>
+																Submit Score
+															</a>
+														}
 													</div>
 												</div>
 
@@ -198,7 +210,7 @@ export default function Competition({ competition, preview }) {
 												{
 													competition.matchResults?.length
 													&& <section>
-												GroupRankingsCard		<h2 className='mt-20 text-2xl md:text-3xl font-bold tracking-tighter leading-tight mx-auto'>
+														GroupRankingsCard		<h2 className='mt-20 text-2xl md:text-3xl font-bold tracking-tighter leading-tight mx-auto'>
 															Latest Results
 														</h2>
 														<div className='mx-auto mt-10 mb-20'>
