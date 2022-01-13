ca3a531abb669824b5a69cf5de667378b16d1cff
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-01-14 00:48:36 +1100

    fix issue when there is no teams

--- pages/competitions/index.js
+++ pages/competitions/index.js
@@ -65,6 +65,6 @@ export async function getStaticProps({ params, preview = false }) {
 			preview,
 			competitions: data,
 		},
-		revalidate: 3600
+		revalidate: 60
 	};
 }
--- pages/index.js
+++ pages/index.js
@@ -70,6 +70,6 @@ export async function getStaticProps({ preview = false }) {
 
 	return {
 		props: { preview, allPosts, allPlayers, competittions },
-		revalidate: 3600
+		revalidate: 60
 	};
 }
