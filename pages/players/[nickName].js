fd8b715d9b1e6ecc165b4107110b912e4b0a8f54
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-01-23 03:01:10 +1100

    show profile

--- pages/players/[nickName].js
+++ pages/players/[nickName].js
@@ -174,15 +174,6 @@ export default function Player({ player, preview }) {
 											</div>
 											<div className='w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center'>
 												<div className='py-6 px-3 mt-32 sm:mt-0'>
-													{
-														player.suburb
-														&& <button
-															className='bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150'
-															type='button'
-														>
-															<i class="fas fa-map-marker-alt mx-2"></i> {player.suburb}
-														</button>
-													}
 												</div>
 											</div>
 											<div className='w-full lg:w-4/12 px-4 lg:order-1'>
@@ -224,13 +215,21 @@ export default function Player({ player, preview }) {
 													({player.nickName})
 												</h3>
 											}
-											<div className='text-sm leading-normal mt-0 mb-2 text-gray-400 font-bold uppercase'>
-												<i className='fas fa-home mr-2 text-lg text-gray-400'></i>{' '}
-												Home Club: {player.homeClub ||
-													'Unknown Club'}
+											<div className='text-lg leading-normal mt-0 mb-2 text-gray-400 font-bold'>
+												{
+													player.suburb
+													&& <>
+														Live in {player.suburb},&nbsp;
+													</>
+												}
+
+												{player.homeClub && <>
+													Play at {player.homeClub}
+												</>}
+
 											</div>
 											<div className='mb-20 text-green-900 mt-10 text-6xl font-bold'>
-												{player.avtaPoint}
+												{player.avtaPoint} pt.
 											</div>
 										</div>
 
