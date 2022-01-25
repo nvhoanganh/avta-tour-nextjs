d0ddb90d9bb925ec0a0aec8ac54b2f4ca731d752
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-01-25 11:44:16 +1100

    move to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,21 @@ import '../styles/index.css'
 import '../styles/minimal.css'
 import '../styles/spinner.css'
 import "@fortawesome/fontawesome-free/css/all.min.css"
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>
+        AVTA - Australia Vietnamese Tennis Association
+      </title>
+      <script type="text/javascript" src="/newrelic.js">
+      </script>
+      <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css"></link>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -39,15 +39,6 @@ export default function Index({
 	return (
 		<>
 			<Layout preview={preview}>
-				<Head>
-					<title>
-						AVTA - Australia Vietnamese Tennis Association
-					</title>
-					<script type="text/javascript" src="/newrelic.js">
-					</script>
-					<script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
-					<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css"></link>
-				</Head>
 				<Navbar />
 				<Intro upcomingCompetition={upcomingCompetition} />
 
