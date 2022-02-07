aa4a36c167e9bd476e32eddf8debf4cfe595a8ee
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-02-08 00:30:46 +1100

    get result data

--- pages/competitions/[slug]/index.js
+++ pages/competitions/[slug]/index.js
@@ -15,6 +15,7 @@ import Header from '../../../components/header';
 import PostHeader from '../../../components/post-header';
 import Layout from '../../../components/layout';
 import { downloadTournamentRankingResults, downloadTournamentResults, getAllCompetitionsForHome, getCompetitionBySlug } from '../../../lib/api';
+import { geCompResults } from '../../../lib/backendapi';
 import { db } from '../../../lib/firebase';
 import PostTitle from '../../../components/post-title';
 import Intro from '../../../components/intro';
@@ -357,6 +358,12 @@ export async function getStaticProps({ params, preview = false }) {
       matchResults
     };
   }
+
+  // const matchScores = await geCompResults(data.sys.id);
+  // data = {
+  //   ...data,
+  //   matchScores
+  // };
 
   if (data?.rankingSheet) {
     const groupRanking = await downloadTournamentRankingResults(data.rankingSheet);
