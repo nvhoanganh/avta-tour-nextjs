ac943171d4268040f37bccf34f199fac4e146f04
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-07-25 21:54:52 +1000

    fix loading time slow

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,6 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css"></link>
-          <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"></link>
           <script type="text/javascript" src="/newrelic.js" async />
           <script type="text/javascript" src="/ga.js" async />
           <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js" async />
