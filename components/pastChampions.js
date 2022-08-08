3d16973a90338afbc8f04b00826eb262bb066b23
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-08-09 08:46:23 +1000

    Our Championship Winners

--- components/pastChampions.js
+++ components/pastChampions.js
@@ -8,7 +8,7 @@ export default function pastChampions({ champions }) {
 			<div className='flex flex-wrap justify-center text-center mb-24'>
 				<div className='w-full lg:w-6/12 px-4'>
 					<h2 className='text-4xl font-semibold'>
-						Our Past Champions
+						Our Championship Winners
 					</h2>
 				</div>
 			</div>
