e2ecb42acf063a01b25ee75de6a5567f9bb9ce32
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-08-09 08:45:09 +1000

    our past champion instead of out past champion

--- components/pastChampions.js
+++ components/pastChampions.js
@@ -8,7 +8,7 @@ export default function pastChampions({ champions }) {
 			<div className='flex flex-wrap justify-center text-center mb-24'>
 				<div className='w-full lg:w-6/12 px-4'>
 					<h2 className='text-4xl font-semibold'>
-						Out Past Champions
+						Our Past Champions
 					</h2>
 				</div>
 			</div>
