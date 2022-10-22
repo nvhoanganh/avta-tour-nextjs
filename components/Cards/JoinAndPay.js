eb7d371ac048c6c5565a6186e9bee4aaa231a258
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-10-23 00:53:00 +1100

    add joining fee

--- components/Cards/JoinAndPay.js
+++ components/Cards/JoinAndPay.js
@@ -6,6 +6,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import PlayerAvatar from '../Cards/PlayerAvatar';
 import DateComponent from '../date';
 import Stripepaymentinfo from '../stripepaymentinfo';
+import { db, storage, storageBucketId } from '../../lib/firebase';
+import { setDoc, doc, getDoc, addDoc, collection } from "firebase/firestore";
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -19,60 +21,58 @@ export default function JoinLadder({ ladder, fullProfile }) {
 
   return (
     <>
-      <>
-        <form action={`/api/checkout_ladder?playerId=${fullProfile.uid}&ladder=${ladder.id}&fee=${ladder.stripePriceId}`} method="POST"
-          className="relative flex flex-col min-w-0 break-words mb-6 border-0 justify-center items-center"
-        >
-          <p className="py-2 h1">Join <span className="font-bold">{ladder.name}</span>
-          </p>
+      <form action={`/api/checkout_ladder?playerId=${fullProfile.uid}&ladder=${ladder.id}&fee=${ladder.stripePriceId}`} method="POST"
+        className="relative flex flex-col min-w-0 break-words mb-6 border-0 justify-center items-center"
+      >
+        <p className="py-2 h1">Join <span className="font-bold">{ladder.name}</span>
+        </p>
 
-          <div className='text-center pt-8'>
-            <div className='text-sm leading-normal mt-0 mb-2  font-bold uppercase'>
-              <a href={`https://maps.google.com/?q=${ladder.homeClub}`} target='_blank' className='hover:underline'>
-                <i className='fas fa-map-marker-alt mr-2 text-lg '></i>{' '}
-                {ladder.homeClub}
-              </a>
-            </div>
+        <div className='text-center pt-8'>
+          <div className='text-sm leading-normal mt-0 mb-2  font-bold uppercase'>
+            <a href={`https://maps.google.com/?q=${ladder.homeClub}`} target='_blank' className='hover:underline'>
+              <i className='fas fa-map-marker-alt mr-2 text-lg '></i>{' '}
+              {ladder.homeClub}
+            </a>
           </div>
+        </div>
 
-          <div className='text-center'>
-            <div className='leading-normal mt-0 mb-2 '>
-              <a href={`https://maps.google.com/?q=${ladder.homeClub}`} target='_blank' className='hover:underline'>
-                <DateComponent
-                  dateString={
-                    ladder.startDate
-                  }
-                />
-                {' - '}
-                <DateComponent
-                  dateString={
-                    ladder.endDate
-                  }
-                />
-              </a>
-            </div>
+        <div className='text-center'>
+          <div className='leading-normal mt-0 mb-2 '>
+            <a href={`https://maps.google.com/?q=${ladder.homeClub}`} target='_blank' className='hover:underline'>
+              <DateComponent
+                dateString={
+                  ladder.startDate
+                }
+              />
+              {' - '}
+              <DateComponent
+                dateString={
+                  ladder.endDate
+                }
+              />
+            </a>
           </div>
+        </div>
 
-          <div className='py-5 pb-10 px-5' dangerouslySetInnerHTML={{ __html: ladder?.rule?.replace(/\\n/g, '<br />') }}>
-          </div>
+        <div className='py-5 pb-10 px-5' dangerouslySetInnerHTML={{ __html: ladder?.rule?.replace(/\\n/g, '<br />') }}>
+        </div>
 
-          <button type="submit" role="link" className="bg-purple-500 text-white active:bg-blue-600 font-bold px-8 py-4 rounded shadow hover:shadow-md outline-none focus:outline-none ease-linear transition-all duration-150
+        <button type="submit" role="link" className="bg-purple-500 text-white active:bg-blue-600 font-bold px-8 py-4 rounded shadow hover:shadow-md outline-none focus:outline-none ease-linear transition-all duration-150
     disabled:cursor-wait whitespace-nowrap
              disabled:bg-gray-200">
-            Pay ${ladder.joiningFee}.00 now with Stripe.com
-          </button>
+          Pay ${ladder.joiningFee}.00 now with Stripe.com
+        </button>
 
-          <Stripepaymentinfo></Stripepaymentinfo>
-        </form>
+        <Stripepaymentinfo></Stripepaymentinfo>
+      </form>
 
-        <p className="text-gray-400 text-sm text-center mb-12">
-          <Link href={`/ladders/${ladder.id}`}><a
-            className='underline ml-2'
-          >
-            Go Back
-          </a></Link>
-        </p>
-      </>
+      <p className="text-gray-400 text-sm text-center mb-12">
+        <Link href={`/ladders/${ladder.id}`}><a
+          className='underline ml-2'
+        >
+          Go Back
+        </a></Link>
+      </p>
     </>
   );
 }
--- components/editladderform.js
+++ components/editladderform.js
@@ -1,7 +1,10 @@
 import { useForm } from "react-hook-form";
+import Link from 'next/link'
 import SaveButton from './savebutton';
+import { useState } from 'react'
 
 export default function EditLadderForm({ onSubmit, currentValue, saving }) {
+  const [showHelp, setShowHelp] = useState(false);
   const { register, reset, handleSubmit, watch, formState: { errors } } = useForm({
     defaultValues: {
       ...currentValue,
@@ -65,23 +68,7 @@ export default function EditLadderForm({ onSubmit, currentValue, saving }) {
                 </label>
               </div>
             </div>
-            <div className="w-full lg:w-6/12 sm:px-4">
-              <div className="relative w-full mb-3">
-                <label className="block uppercase text-gray-600 text-xs font-bold mb-2" htmlFor="grid-password">
-                  Joining Fee
-                </label>
-                <select className="appearance-none
-        border px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" aria-label="Default select example"
-                  {...register("joiningFee", { required: true })}
-                >
-                  <option value={10}>$10</option>
-                  <option value={20}>$20</option>
-                  <option value={30}>$30</option>
-                  <option value={40}>$40</option>
-                  <option value={50}>$50</option>
-                </select>
-              </div>
-            </div>
+
             <div className="w-full lg:w-6/12 sm:px-4">
               <div className="relative w-full mb-3">
                 <label className="block uppercase text-gray-600 text-xs font-bold mb-2" htmlFor="grid-password">
@@ -107,6 +94,62 @@ export default function EditLadderForm({ onSubmit, currentValue, saving }) {
                 <textarea type="text" className="border px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" rows="4"
                   {...register("rule", { required: true })}></textarea>
               </div>
+            </div>
+          </div>
+
+          <div className="w-full lg:w-6/12 sm:px-4">
+            <div className="relative w-full mb-3">
+              <label className="block uppercase text-gray-600 text-xs font-bold mb-2" htmlFor="grid-password">
+                Joining Fee (AUD)
+                <span className="underline cursor-pointer" onClick={() => setShowHelp(!showHelp)}><i className="far fa-question-circle ml-2 text-lg" ></i> What is this joining fee?</span>
+              </label>
+              <select className="appearance-none
+        border px-3 py-3 placeholder-gray-300 text-gray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" aria-label="Default select example"
+                {...register("joiningFee", { required: true })}
+              >
+                <option value={10}>$10</option>
+                <option value={20}>$20</option>
+                <option value={30}>$30</option>
+                <option value={40}>$40</option>
+                <option value={50}>$50</option>
+              </select>
+
+              {showHelp &&
+                <div className="py-3 my-4 border rounded shadow-xl px-3 bg-gray-50">
+                  <p>
+                    To join the ladder, player will pay the amount above using <a
+                      className='text-blue-600 hover:text-gray-800 font-semibold'
+                      href='https://stripe.com'
+                    >
+                      Stripe.com
+                    </a>. At the end of the ladder, <a
+                      className='text-blue-600 hover:text-gray-800 font-semibold'
+                      href='mailto:nvhoanganh1909@gmail.com'
+                    >
+                      email us
+                    </a> and provide your Australian Bank Account details and we will transfer the total registration amount less <strong>3% total + 30c per joined user</strong> platform fee.
+                  </p>
+
+                  <p className='pt-5'><strong>Note:</strong> Your email is used to identify the ladder you created. Make sure you use the same email address you used to sign up with AVTATour.com.</p>
+
+                  <p className='pt-5'>How you spend the fund is up to you. For example, you can use it to purchase Trorphies for Ladder winners</p>
+
+                  <p className="pt-5">
+                    For example, if joining fee is $50 and you have 10 people joined. Total amount you will receive will be
+
+                    <p className="pt-5 italic">
+                      $500 - (3% * $500 + $0.30*10)  = $482.00.
+                    </p>
+
+                    <p className="pt-5">
+                      <button className="bg-gray-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="button"
+                        onClick={() => setShowHelp(false)}
+                      >
+                        Got it!
+                      </button>
+                    </p>
+                  </p>
+                </div>}
             </div>
           </div>
 
