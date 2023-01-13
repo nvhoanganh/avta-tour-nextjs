d0a6a51ccadf4bc8e285dd93c9cad7aeeeeecdeb
Anthony Nguyen <nvhoanganh1909@gmail.com>
2023-01-13 17:51:00 +1100

    show h2h

--- components/possibleMatches.js
+++ components/possibleMatches.js
@@ -37,37 +37,54 @@ export default function PossibleMatches({ matches, filter }) {
 					match.pointDiff > 20 && match.pointDiff <= 30 ? '20-30' : '30-40'}</div> */}
 				{
 					match.team1.point !== match.team2.point && parseInt(match.team1.point) > parseInt(match.team2.point)
-						? <div>
-							<div>
-								<span className={_isPlayerInFilter(filter, match.team1.player1) ? 'font-bold text-blue-800' : ''}>{match.team1.player1.trim()}</span> -&nbsp;
-								<span className={_isPlayerInFilter(filter, match.team1.player2) ? 'font-bold text-blue-800' : ''}>{match.team1.player2.trim()}</span>&nbsp;
-								[<span>{match.team1.point}</span>]
-								<span className='bg-green-400 px-1 rounded mx-1 text-white'>{match.team1.winTogether.length}</span>
-								<span className='bg-red-400 px-1 rounded mx-1 text-white'>{match.team1.lostTogether.length}</span>
+						?
+						<div className='flex flex-row items-center justify-between w-full'>
+							<div className='grow'>
+								<div>
+									<span className={_isPlayerInFilter(filter, match.team1.player1) ? 'font-bold text-blue-800' : ''}>{match.team1.player1.trim()}</span> -&nbsp;
+									<span className={_isPlayerInFilter(filter, match.team1.player2) ? 'font-bold text-blue-800' : ''}>{match.team1.player2.trim()}</span>&nbsp;
+									[<span>{match.team1.point}</span>]
+									<span className='bg-green-400 px-1 rounded mx-1 text-white'>{match.team1.winTogether?.length}</span>
+									<span className='bg-red-400 px-1 rounded mx-1 text-white'>{match.team1.lostTogether?.length}</span>
+								</div>
+								<div>
+									<span className={_isPlayerInFilter(filter, match.team2.player1) ? 'font-bold text-blue-800' : ''}>{match.team2.player1.trim()}</span> -&nbsp;
+									<span className={_isPlayerInFilter(filter, match.team2.player2) ? 'font-bold text-blue-800' : ''}>{match.team2.player2.trim()}</span>&nbsp;
+									[<span>{match.team2.point}</span>]
+									<span className='bg-green-400 px-1 rounded mx-1 text-white'>{match.team2.winTogether?.length}</span>
+									<span className='bg-red-400 px-1 rounded mx-1 text-white'>{match.team2.lostTogether?.length}</span>
+								</div>
 							</div>
-							<div>
-								<span className={_isPlayerInFilter(filter, match.team2.player1) ? 'font-bold text-blue-800' : ''}>{match.team2.player1.trim()}</span> -&nbsp;
-								<span className={_isPlayerInFilter(filter, match.team2.player2) ? 'font-bold text-blue-800' : ''}>{match.team2.player2.trim()}</span>&nbsp;
-								[<span>{match.team2.point}</span>]
-								<span className='bg-green-400 px-1 rounded mx-1 text-white'>{match.team2.winTogether.length}</span>
-								<span className='bg-red-400 px-1 rounded mx-1 text-white'>{match.team2.lostTogether.length}</span>
-							</div>
+							{
+								match.team1Won?.length > 0 || match.team1Lost?.length > 0
+									? <div className='grow-0 text-right font-semibold'>
+										<span>{match.team1Won?.length}-{match.team1Lost?.length}</span>
+									</div>
+									: null
+							}
 						</div>
-						: <div>
-							<div>
-								<span className={_isPlayerInFilter(filter, match.team2.player1) ? 'font-bold text-blue-800' : ''}>{match.team2.player1.trim()}</span> -&nbsp;
-								<span className={_isPlayerInFilter(filter, match.team2.player2) ? 'font-bold text-blue-800' : ''}>{match.team2.player2.trim()}</span>&nbsp;
-								[<span>{match.team2.point}</span>]
-								<span className='bg-green-400 px-1 rounded mx-1 text-white'>{match.team2.winTogether.length}</span>
-								<span className='bg-red-400 px-1 rounded mx-1 text-white'>{match.team2.lostTogether.length}</span>
+						: <div className='flex flex-row items-center justify-between w-full'>
+							<div className='grow'>
+								<div>
+									<span className={_isPlayerInFilter(filter, match.team2.player1) ? 'font-bold text-blue-800' : ''}>{match.team2.player1.trim()}</span> -&nbsp;
+									<span className={_isPlayerInFilter(filter, match.team2.player2) ? 'font-bold text-blue-800' : ''}>{match.team2.player2.trim()}</span>&nbsp;
+									[<span>{match.team2.point}</span>]
+									<span className='bg-green-400 px-1 rounded mx-1 text-white'>{match.team2.winTogether?.length}</span>
+									<span className='bg-red-400 px-1 rounded mx-1 text-white'>{match.team2.lostTogether?.length}</span>
+								</div>
+								<div>
+									<span className={_isPlayerInFilter(filter, match.team1.player1) ? 'font-bold text-blue-800' : ''}>{match.team1.player1.trim()}</span> -&nbsp;
+									<span className={_isPlayerInFilter(filter, match.team1.player2) ? 'font-bold text-blue-800' : ''}>{match.team1.player2.trim()}</span>&nbsp;
+									[<span>{match.team1.point}</span>]
+									<span className='bg-green-400 px-1 rounded mx-1 text-white'>{match.team1.winTogether?.length}</span>
+									<span className='bg-red-400 px-1 rounded mx-1 text-white'>{match.team1.lostTogether?.length}</span>
+								</div>
 							</div>
-							<div>
-								<span className={_isPlayerInFilter(filter, match.team1.player1) ? 'font-bold text-blue-800' : ''}>{match.team1.player1.trim()}</span> -&nbsp;
-								<span className={_isPlayerInFilter(filter, match.team1.player2) ? 'font-bold text-blue-800' : ''}>{match.team1.player2.trim()}</span>&nbsp;
-								[<span>{match.team1.point}</span>]
-								<span className='bg-green-400 px-1 rounded mx-1 text-white'>{match.team1.winTogether.length}</span>
-								<span className='bg-red-400 px-1 rounded mx-1 text-white'>{match.team1.lostTogether.length}</span>
-							</div>
+							{
+								match.team1Lost?.length > 0 || match.team1Won?.length > 0
+									? <div className='grow-0 text-right font-semibold'><span>{match.team1Lost?.length}-{match.team1Won?.length}</span></div>
+									: null
+							}
 						</div>
 				}
 			</div>
