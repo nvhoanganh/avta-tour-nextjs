b012329e95534d8214c9c8c780796396e623a088
Anthony Nguyen <nvhoanganh1909@gmail.com>
2023-06-06 16:48:24 +1000

    render inline assets

--- components/post-body.js
+++ components/post-body.js
@@ -1,9 +1,41 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+
+// Create a bespoke renderOptions object to target BLOCKS.EMBEDDED_ENTRY (linked block entries e.g. code blocks)
+// INLINES.EMBEDDED_ENTRY (linked inline entries e.g. a reference to another blog post)
+// and BLOCKS.EMBEDDED_ASSET (linked assets e.g. images)
+
+// https://www.contentful.com/blog/rendering-linked-assets-entries-in-contentful/
+function renderOptions(links) {
+	// create an asset map
+	const assetMap = new Map();
+	// loop through the assets and add them to the map
+	for (const asset of links.assets.block) {
+		assetMap.set(asset.sys.id, asset);
+	}
+
+	return {
+		// other options...
+		renderNode: {
+			[BLOCKS.EMBEDDED_ASSET]: (node, next) => {
+				// find the asset in the assetMap by ID
+				const asset = assetMap.get(node.data.target.sys.id);
+
+				// render the asset accordingly
+				return (
+					<img src={asset.url} alt="My image alt text" />
+				);
+			},
+		},
+	};
+}
+
 
 export default function PostBody({ content }) {
+	console.log("🚀 ~ file: post-body.js:24 ~ PostBody ~ content:", JSON.stringify(content.links, null, 2))
 	return (
 		<div className='prose text-lg max-w-none'>
-			{documentToReactComponents(content.json)}
+			{documentToReactComponents(content.json, renderOptions(content.links))}
 		</div>
 	);
 }
