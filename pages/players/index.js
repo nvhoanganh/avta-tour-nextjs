4122df04b876842582c31ebdf3f1e889b0fcd87a
Anthony Nguyen <nvhoanganh1909@gmail.com>
2024-08-23 23:52:39 +1000

    increase width on the UI

--- pages/players/index.js
+++ pages/players/index.js
@@ -46,7 +46,7 @@ export default function Players({ allPlayers, preview }) {
 							bgImg='https://unsplash.com/photos/RNiK93wcz-U/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8dGVubmlzJTIwcGxheWVyc3x8MHx8fHwxNjQyMDUxNDk2&force=true&w=1920'
 						>
 							<div className='w-full mb-12'>
-								<div className='hidden container mx-auto md:block px-4'>
+								<div className='hidden mx-auto md:block px-52'>
 									<PlayersTable
 										players={allPlayers}
 										user={user}
